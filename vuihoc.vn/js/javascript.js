693d79f6d6e64b1ddf49d31a2b46d477669a90d3
NgatDo <dongat2501@gmail.com>
2021-03-01 21:43:07 +0700

    Add HTML tags

--- vuihoc.vn/js/javascript.js
+++ vuihoc.vn/js/javascript.js
@@ -13,4 +13,19 @@ $(document).ready(function () {
         $('.' + activeContentWrapper).removeClass(contentActiveClass);
         $('.' + activeContent).addClass(contentActiveClass);
     });
+    
+    $('.tab-row').click(function () {
+        var activeClass = $(this).attr('data-class-active');
+        var accessClass = $(this).attr('data-class-access');
+
+        var activeContent = $(this).attr('data-active-content');
+        var contentActiveClass = $(this).attr('data-content-class-active');
+        var activeContentWrapper = $(this).attr('data-active-content-wrapper');
+
+        $('.' + accessClass).removeClass(activeClass);
+        $(this).addClass(activeClass);
+
+        $('.' + activeContentWrapper).removeClass(contentActiveClass);
+        $('.' + activeContent).addClass(contentActiveClass);
+    });
 });
